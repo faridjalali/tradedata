56b6e2bfd0c45b1d8f9ec089f984f721974f28b6
Farid Jalali <farid.jalali@gmail.com>
2026-02-05 21:12:55 -0800

    a

--- public/app.js
+++ public/app.js
@@ -532,7 +532,10 @@ function renderTradingViewChart(ticker) {
         "toolbar_bg": "#f1f3f6",
         "enable_publishing": false,
         "allow_symbol_change": false,
-        "container_id": "tradingview_chart"
+        "container_id": "tradingview_chart",
+        "studies": [
+            "Moving Average Ribbon"
+        ]
     });
 }
 
